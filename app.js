0e12b817cb4c8b6a13bbcfdd49e5edf659403429
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-07-25 11:07:46 +0300

    Add a css file and link.

---
+++ app.js
@@ -0,0 +1,20 @@
+//DOM manipulation
+
+
+//GetElementByID()
+
+const title = document.getElementById('main-heading');
+
+console.log(title);
+
+//GetElementByClassName()
+
+
+//GetElementByTagNAme()
+
+
+//QuerySelector()
+
+
+//QuerySelectorAll()
+
